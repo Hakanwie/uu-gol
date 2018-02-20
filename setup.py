e945bb34b9b1491c05b16e9babbe84db2fa10de2
Håkan Wieslander <hakan.wieslander@it.uu.se>
2018-02-20 12:01:43 +0100

    Added game of life files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name = 'uu-gol',
+    version='1.0',
+    description='Example package of game of life',
+    url='https://github.com/uu-python/travis-fib',
+    author='Håkan Wieslander',
+    author_email='hakan.wieslander@it.uu.se',
+    py_modules=['gol'],
+    packages=[])
